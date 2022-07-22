e4cbb324ca4692f1a276db9329fb562ad7b511e6
Nicholas Rogers <nickrogersthegreat@gmail.com>
2022-07-22 09:48:26 -0600

    commit tree df765803a3f583b53b91f46654f73516e8979467

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function (toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`;
+}
+function wrapAdjective(visualFlare = "*") {
+    const encouragingPromptFunction =  function (howIFeel = "special") {
+        return `You are ${visualFlare}${howIFeel}${visualFlare}!`;
+    };
+    return encouragingPromptFunction;
+};
